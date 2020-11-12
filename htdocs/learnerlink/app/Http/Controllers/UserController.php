b17bed93a3d3db605c4b476a8cb4c3d378babd3d
chika.masuda <c.m.062820@gmail.com>
2020-11-12 16:35:56 +0900

    トップページのレイアウト変更

--- htdocs/learnerlink/app/Http/Controllers/UserController.php
+++ htdocs/learnerlink/app/Http/Controllers/UserController.php
@@ -15,13 +15,12 @@ use Illuminate\Support\Facades\Validator;
 use Intervention\Image\Facades\Image;
 use App\Reaction;
 
-
 class UserController extends Controller
 {
     public function show($id)
     {
         $user = User::findorFail($id);
-        
+
         return view('users.show', compact('user'));
     }
     
@@ -34,7 +33,6 @@ class UserController extends Controller
     
     public function update($id, ProfileRequest $request)
     {
-        
         $this->validate($request, [
             'name' => 'required|string|max:15',
             'email' => 'required|string|email|max:255',
@@ -74,78 +72,10 @@ class UserController extends Controller
     
     public function index()
     {
-        $got_reaction_ids = Reaction::where([
-            ['like_id', Auth::id()], //like_idがログイン中のユーザーIDになる
-            ])->pluck('user_id');
-            
-        $like_users = User::whereIn('id', $got_reaction_ids)->get();
+        $users = User::orderBy('id', 'desc')->paginate(20);
         
-        $like_users_count = count($like_users);
-    
-        return view('users.index', compact('like_users', 'like_users_count'));
-    }
-    
-    public function search(Request $request) {
-        $keyword_name = $request->name;
-        $keyword_sex = $request->sex;
-        $keyword_language = $request->language;
-        $keyword_address = $request->address;
-        $name = User::where('name')->get();
-       
-        if(!empty($keyword_name)){
-            $query = User::query();
-            $users = $query->where('name', 'like', '%' .$keyword_name. '%')->get();
-            return view('/search')->with([
-                'users' => $users,
-
+        return view('users.index',[
+            'users' => $users,
             ]);
-        } 
-        
-        
-        elseif(!empty($keyword_language)){
-            $query = User::query();
-            $users = $query->where('language', 'like', '%' .$keyword_language. '%')->get();
-            $message = "言語の検索が完了しました";
-            return view('/search')->with([
-                'users' => $users,
-            ]);
-        }
-        
-        elseif(!empty($keyword_address)){
-            $query = User::query();
-            $users = $query->where('address', 'like', '%' .$keyword_address. '%')->get();
-            $message = "住所の検索が完了しました";
-            return view('/search')->with([
-                'users' => $users,
-            ]);
-        }
-    
-        elseif($keyword_sex == 1){
-            
-            $query = User::query();
-            $users = $query->where('sex', '男性')->get();
-            $message = "男性の検索が完了しました";
-              return view('/search')->with([
-                  'users' => $users,
-              ]);
-        }
-        
-        elseif($keyword_sex == 2){
-            
-            $query = User::query();
-            $users = $query->where('sex', '女性')->get();
-            $message = "女性の検索が完了しました";
-              return view('/search')->with([
-                  'users' => $users,
-              ]);
-        }
-        
-        else {
-            $users = null;
-            $message = "検索結果はありません";
-            return view('search')->with([
-                'message' => $message,
-            ]);
-        }
     }
 }
