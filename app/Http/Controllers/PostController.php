b2da090bb7bb43ff11b4f634a34c58ecea17059a
chikamasuda <c.m.062820@gmail.com>
2021-01-20 04:58:52 +0900

    投稿の表示順の調整

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all() ;
-        $posts = Post::orderBy('id', 'desc')->paginate(5);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
         return view('posts.index',['posts' => $posts]);
     }
 
